980e20e21cf251792e7ea29b5cfb41c8ded54763
Zack Underwood <Underwoodzack159@gmail.com>
2025-06-12 14:27:43 -0700

    Making nav bar

--- legoWebsite/src/App.tsx
+++ legoWebsite/src/App.tsx
@@ -1,23 +1,15 @@
 import TopoBackground from "./components/TopoBackground";
-
-// function App() {
-//   return (
-//     <>
-//       <TopoBackground />
-//       {/* ...your other components... */}
-//     </>
-//   );
-// }
-
-//import WireframeBackground from "./components/TopoBackground";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <>
       <TopoBackground />
-      <div style={{ color: "#fff", padding: 40 }}>
-        <h1>Cool App with Wireframe Background</h1>
-      </div>
+      
+
+      <NavBar />
+
+      
     </>
   );
 }
---
+++ legoWebsite/src/components/NavBar.js
@@ -0,0 +1,15 @@
+export default function NavBar() {
+    return <nav className="nav"> 
+    
+        <a href="/" className="site-title">Site Name</a>
+
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/Lego">Custom Lego</a></li>
+            <li><a href="/Other Customs">Other Customs</a></li>
+            <li><a href="/About">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+        </ul>
+
+    </nav>
+}
